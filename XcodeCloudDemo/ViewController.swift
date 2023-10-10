0e94fe16a016dd754e6aacc8d20749b5d4f08ca9
ahzamghouri <137301677+ahzamghouri@users.noreply.github.com>
2023-10-10 15:40:03 +0500

    Initial Commit

---
+++ XcodeCloudDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeCloudDemo
+//
+//  Created by AST M1 (2) on 10/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
